4e5fecdc38cfcd9a444090cd4ec1c4c89f4df1f1
bytebeardigital <hello@bytebeardigital.com>
2022-04-06 21:13:43 -0400

    mobile-nav fix

--- assets/js/scripts/main.js
+++ assets/js/scripts/main.js
@@ -1,31 +1,17 @@
-const common = (($) => {
-  "use strict";
-
-  /**
-   * Some function
-   *
-   * @since   1.0.0
-   */
-  const someFunction = () => {
-    // do something
-    console.log("testing 1,2,3");
+jQuery(document).ready(function($) {
+  var alterClass = function() {
+    var ww = document.body.clientWidth;
+    if (ww < 600) {
+      $('.mobile-nav').removeClass('hidden');
+      $('.desktop-nav').addClass('hidden');
+    } else if (ww >= 601) {
+      $('.mobile-nav').addClass('hidden');
+      $('.desktop-nav').removeClass('hidden');
+    };
   };
-
-  /**
-   * Fire events on document ready and bind other events
-   *
-   * @since   1.0.0
-   */
-  const ready = () => {
-    someFunction();
-  };
-
-  /**
-   * Only expose the ready function to the world
-   */
-  return {
-    ready: ready,
-  };
-})(jQuery);
-
-jQuery(common.ready);
+  $(window).resize(function(){
+    alterClass();
+  });
+  //Fire it when the page first loads:
+  alterClass();
+});
